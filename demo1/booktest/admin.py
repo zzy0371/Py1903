332cbd8c82ddeeff190497017b33bd31b1e3575c
zzy0371 <496575233@qq.com>
2019-06-05 09:47:23 +0800

    添加后台管理

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from .models import BookInfo,HeroInfo
 """
 django自带强大的后台管理
 """
 # Register your models here.
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
 
--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -9,9 +9,15 @@ class BookInfo(models.Model):
     title = models.CharField(max_length=20)
     pub_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.title
+
 class HeroInfo(models.Model):
     name = models.CharField(max_length=20)
     gender = models.BooleanField(default=True)
     content = models.CharField(max_length=100)
     # book 作为外键关联到bookinfo表
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
