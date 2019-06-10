a4cedfb20f40a703a073bdffd52419fd8c1a5eb6
zzy0371 <496575233@qq.com>
2019-06-10 16:54:26 +0800

    复杂查询

--- demo1/relationtest/admin.py
+++ demo1/relationtest/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Temp)
+admin.site.register(AreaInfo)
--- demo1/relationtest/models.py
+++ demo1/relationtest/models.py
@@ -48,4 +48,13 @@ class Temp(models.Model):
         return self.title
 
 
+class AreaInfo(models.Model):
+    title = models.CharField(max_length=15)
+    parent = models.ForeignKey("self",on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+
 
