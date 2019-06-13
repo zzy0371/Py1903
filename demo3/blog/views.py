33861443db0d95b65baaa58c4eb1e760f32408d6
zzy0371 <496575233@qq.com>
2019-06-13 11:52:17 +0800

    引入静态模板文件

--- demo3/blog/views.py
+++ demo3/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import View,ListView,DetailView
 # Create your views here.
+
+class IndexView(View):
+    def get(self,req):
+        return render(req,"blog/index.html")
+
+class SingleView(View):
+    def get(self,req,id):
+        return render(req, "blog/single.html")
--- demo3/demo3/settings.py
+++ demo3/demo3/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'demo3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"template")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
